f2ce7f6415a0e4e6e4e29be8286f7bbaacff0941
Andrey Kulaga <kulagaad@gmail.com>
2023-03-28 00:51:14 +0400

    Update to reas JSON raw data and configuration. New Header and Footer Configuration of each column of data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,105 +5,107 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.io.*;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
 
-        //load configuration
-        ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(Configuration.class, new ConfigurationDeserializer());
-        mapper.registerModule(module);
 
-        Configuration configuration;
+        //Create Configuration, read NewJsonConfigurationRequest and import it to Configuration;
+        Configuration configuration = new Configuration();
+        NewJsonConfigurationRequest nJCR = new NewJsonConfigurationRequest();
 
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./configuration.txt"))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./NewJsonConfigurationRequestModel.txt"))) {
             String line = bufferedReader.readLine();
-            configuration = mapper.readValue(line, Configuration.class);
+            String myJsonString = "";
+            
+            while (line != null) {
+                myJsonString = myJsonString.concat(line);
+                line = bufferedReader.readLine();
+            }
+            
+            ObjectMapper om = new ObjectMapper();    
+            nJCR = om.readValue(myJsonString, NewJsonConfigurationRequest.class);
+            configuration.importNewJsonConfigurationRequest(nJCR);
+            
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        
+        
+        
+        //Read JSON response 
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(JsonResponse.class, new JsonResponseDeserializer());
+        mapper.registerModule(module);
+        
+        JsonResponse jsonResponse = new JsonResponse();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./JsonResponseWithData.txt"))) {
+            String line = bufferedReader.readLine();
+            String myJsonString = "";
+            
+            while (line != null) {
+                myJsonString = myJsonString.concat(line);
+                line = bufferedReader.readLine();
+            }
+            
+            jsonResponse = mapper.readValue(myJsonString, JsonResponse.class);
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
 
-        //create array list of transactions
-        ArrayList<Transaction> transactions = new ArrayList<>();
+        //create array list of all columns names
+        ArrayList<String> columnNames = jsonResponse.createColumnNames();
+
+        //create HashMap of column names for table head
+        HashMap<String, String> columnNamesForTableHead = jsonResponse.createColumnNamesForTableHead();
+
+        //create hash map of types of columns
+        HashMap<String, String> hashMapOfTypes = jsonResponse.createHashMapOfTypes();
+
 
         //create array list that we will need to calculate cell width
         HashMap<String, Integer> textLengths = new HashMap<>();
-
-        //create hash map to store types of columns
-        HashMap<String, String> hashMapOfTypes;
-
-        //create array list to store all columns names
-        ArrayList<String> columnNames = new ArrayList<>();
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./Raw_Data.txt"))) {
-
-            String[] firstLine = bufferedReader.readLine().split("\\|");
-            columnNames.addAll(Arrays.asList(firstLine));
-
-            //convert column names to sentence cased
-            for (int i=0; i< columnNames.size(); i++) {
-                String tempString = columnNames.get(i);
-                tempString = tempString.toLowerCase().replaceAll("_", " ");
-                tempString = tempString.substring(0,1).toUpperCase().concat(tempString.substring(1));
-                columnNames.set(i, tempString);
+        
+        //fill array that we will need to calculate cell width
+        for (int i = 0; i < columnNames.size(); i++) {
+            if (columnNames.get(i).length() > configuration.getMaxCharactersInTextLine()) {
+                textLengths.put(columnNames.get(i), configuration.getMaxCharactersInTextLine());
+            } else {
+                textLengths.put(columnNames.get(i), columnNames.get(i).length());
             }
-
-            //fill array that we will need to calculate cell width
-            for (int i = 0; i < columnNames.size(); i++) {
-                if (columnNames.get(i).length() > configuration.getMaxCharactersInTextLine()) {
-                    textLengths.put(columnNames.get(i), configuration.getMaxCharactersInTextLine());
-                } else {
-                    textLengths.put(columnNames.get(i), columnNames.get(i).length());
-                }
-            }
-
-            //define fields types
-            String line = bufferedReader.readLine();
-            hashMapOfTypes = TransactionParser.defineFieldsTypes(line, columnNames);
-
-
-            //fill list of transactions
-            long transactionNumber = 1;
-            while (line != null) {
-
-                String[] strings = line.split("\\|");
-
-                try {
-                    Transaction transaction = TransactionParser.parseTextLineIntoTransaction(transactionNumber, line, hashMapOfTypes, columnNames, configuration.getWhatColumnsToHide());
-                    transactions.add(transaction);
-                    //check length and keep the biggest one to calculate cell width latter
-                    for (String string:columnNames) {
-                        int length = transaction.getAllValuesAsString().get(string).length();
-                        if (length > textLengths.get(string) && length <= configuration.getMaxCharactersInTextLine()) {
-                            textLengths.replace(string, length);
-                        }
-                    }
-                } catch (ArrayIndexOutOfBoundsException | DateTimeParseException e) {
-                    throw new RuntimeException("check the line number " + transactionNumber + ". It has not the same quantity of fields as the first one or some fields has other type");
-                }
-                //read next line
-                transactionNumber++;
-                line = bufferedReader.readLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+        
+        //create array list of transactions and extract from JsonResponse
+        ArrayList<Transaction> transactions = jsonResponse.extractTransactions(textLengths, configuration);
 
+    
 
         //clean textLengths from columns that are hidden
         for (String string : configuration.getWhatColumnsToHide()) {
             textLengths.remove(string);
         }
 
-
+        
+        
         //clean array list of column names from hidden columns
         for (String string : configuration.getWhatColumnsToHide()) {
             columnNames.remove(string);
+        }
+        
+        //warning message when there are more columns then max in configuration
+        if (columnNames.size() > configuration.getMaxColumnsAllowed()) {
+            JOptionPane optionPane = new JOptionPane("There are more columns in your report than column limit",JOptionPane.WARNING_MESSAGE);
+            JDialog dialog = optionPane.createDialog("Warning!");
+            dialog.setAlwaysOnTop(true); // to show top of all other application
+            dialog.setVisible(true); // to visible the dialog
         }
 
         //clean map of types from hidden columns
@@ -111,11 +113,10 @@ public class Main {
             hashMapOfTypes.remove(string);
         }
 
-
         //create preview
         if (configuration.isPreview()) {
             try (PDDocument doc = new PDDocument()) {
-                Pdf pdf = new Pdf(doc, configuration, columnNames, textLengths, hashMapOfTypes);
+                Pdf pdf = new Pdf(doc, configuration, columnNames, textLengths, hashMapOfTypes, columnNamesForTableHead);
                 pdf.addNewPage();
                 pdf.addHeadOfTable();
 
@@ -168,7 +169,7 @@ public class Main {
                             for (int k = 0; k <= columnPlace; k++) {
                                 //name of subgroupForSubtotal get from array of configuration.getColumnsToGroupBy starting from the end of the array
                                 String subgroupForSubtotal = configuration.getColumnsToGroupBy().get(configuration.getColumnsToGroupBy().size()-k-1);
-                                pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString().get(subgroupForSubtotal),
+                                pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString(configuration).get(subgroupForSubtotal),
                                         subtotals[k], hashMapOfTypes);
                                 subtotals[k] = new Subtotal(transactions.get(0));
                             }
@@ -189,7 +190,7 @@ public class Main {
                         for (int k = 0; k < subtotals.length - 1; k++) {
                             //name of subgroupForSubtotal get from array of configuration.getColumnsToGroupBy starting from the end of the array
                             String subgroupForSubtotal = configuration.getColumnsToGroupBy().get(configuration.getColumnsToGroupBy().size()-k-1);
-                            pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString().get(subgroupForSubtotal),
+                            pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString(configuration).get(subgroupForSubtotal),
                                     subtotals[k], hashMapOfTypes);
                         }
                     }
@@ -211,7 +212,7 @@ public class Main {
         //create result PDF document
         if (configuration.isPdfExport()) {
             try (PDDocument doc = new PDDocument()) {
-                Pdf pdf = new Pdf(doc, configuration, columnNames, textLengths, hashMapOfTypes);
+                Pdf pdf = new Pdf(doc, configuration, columnNames, textLengths, hashMapOfTypes, columnNamesForTableHead);
                 pdf.addNewPage();
                 pdf.addHeadOfTable();
 
@@ -257,7 +258,7 @@ public class Main {
                             for (int k = 0; k <= columnPlace; k++) {
                                 //name of subgroupForSubtotal get from array of configuration.getColumnsToGroupBy starting from the end of the array
                                 String subgroupForSubtotal = configuration.getColumnsToGroupBy().get(configuration.getColumnsToGroupBy().size()-k-1);
-                                pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString().get(subgroupForSubtotal),
+                                pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString(configuration).get(subgroupForSubtotal),
                                         subtotals[k], hashMapOfTypes);
                                 subtotals[k] = new Subtotal(transactions.get(0));
                             }
@@ -277,7 +278,7 @@ public class Main {
                     for (int k = 0; k < subtotals.length - 1; k++) {
                         //name of subgroupForSubtotal get from array of configuration.getColumnsToGroupBy starting from the end of the array
                         String subgroupForSubtotal = configuration.getColumnsToGroupBy().get(configuration.getColumnsToGroupBy().size()-k-1);
-                        pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString().get(subgroupForSubtotal),
+                        pdf.addSubtotalRow(subgroupForSubtotal + ": " + transactions.get(j).getAllValuesAsString(configuration).get(subgroupForSubtotal),
                                 subtotals[k], hashMapOfTypes);
                     }
                     //add total
